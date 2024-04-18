db1de6b7ac81742487ff87f8f3534685c952d436
Dhanuka <thaveesha222@gmail.com>
2024-04-18 20:03:17 +0530

    Updating model and invoker

--- commentary_predictor/model_invoker.py
+++ commentary_predictor/model_invoker.py
@@ -4,8 +4,27 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 from flask import Flask, request, jsonify
+import joblib
+import nltk
+from nltk.corpus import stopwords
+import string
+import re
+
 
 app = Flask(__name__)
+
+# Function to preprocess text
+def preprocess(text):
+    text = text.lower()
+    text = text.strip()
+    text = re.compile('<.*?>').sub('', text)
+    text = re.compile('[%s]' % re.escape(string.punctuation)).sub(' ', text)
+    text = re.sub('\s+', ' ', text)
+    text = re.sub(r'\[[0-9]*\]',' ',text)
+    text = re.sub(r'[^\w\s]', '', str(text).lower().strip())
+    text = re.sub(r'\d',' ',text)
+    text = re.sub(r'\s+',' ',text)
+    return text
 
 def load_model_and_tokenizer(model_path, tokenizer_path):
     # Load the model
@@ -21,24 +40,21 @@ def load_model_and_tokenizer(model_path, tokenizer_path):
 def predict_run_type():
     commentary = request.json.get('commentary')
 
-    model_path = "./run_type_prediction_model.h5"
-    tokenizer_path = "./run_type_tokenizer.pickle"
+    # Load TF-IDF vectorizer
+    tfidf_vectorizer = joblib.load('./tfidf_vectorizer_run_types.joblib')
 
-    model, tokenizer = load_model_and_tokenizer(model_path, tokenizer_path)
+    # Load SVM model
+    clf = joblib.load('./run_type_prediction_model.joblib')
 
-    # Tokenize the commentary
-    commentary_seq = tokenizer.texts_to_sequences([commentary])
-    commentary_seq = pad_sequences(commentary_seq, maxlen=15, padding='post')
+    preprocessed_text = preprocess(commentary)
+    vectorized_text = tfidf_vectorizer.transform([preprocessed_text])
+    prediction = clf.predict(vectorized_text)
 
-    # Predict runs
-    predicted_runs = model.predict(commentary_seq)
+    prediction = int(prediction[0])
 
-    # Round the predicted runs to the nearest integer
-    rounded_predicted_runs = np.round(predicted_runs).astype(int)
+    return jsonify({'predicted_runs': prediction})
 
-    rounded_predicted_runs_list = rounded_predicted_runs.tolist()
 
-    return jsonify({'predicted_runs': rounded_predicted_runs_list})
 
 @app.route('/predict_run_count', methods=['POST'])
 def predict_run_count():
